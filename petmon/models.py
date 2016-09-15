7d48f8712f9da8ee24f84237e8179bdc70c491d7
TianchenMa <tianchenma@outlook.com>
2016-09-15 11:50:03 +1000

    Modify Pet model

--- petmon/models.py
+++ petmon/models.py
@@ -1,7 +1,61 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
-
 # Create your models here.
+GENDER = {
+    0: u'Male',
+    1: u'Female'
+}
+
+KIND = {
+    0: u'Fire',
+    1: u'Water',
+    2: u'Plant',
+}
+
+ATTRIBUTE = {
+    0: {
+        'hp': 100,
+        'attack': 30,
+        'defence': 10,
+        'speed': 15,
+    },
+    1: {
+        'hp': 100,
+        'attack': 20,
+        'defence': 25,
+        'speed': 10,
+    },
+    3: {
+        'hp': 100,
+        'attack': 25,
+        'defence': 15,
+        'speed': 15,
+    }
+}
+
+
 class User(AbstractUser):
-    intro = models.CharField(max_length=200)
+    gender = models.IntegerField(default=0, choices=GENDER.items())
+    step = models.IntegerField(default=0)
+    intro = models.CharField(max_length=200, null=True)
+
+
+class Pet(models.Model):
+    name = models.CharField(max_length=50, null=False)
+    kind = models.IntegerField(choices=KIND.items())
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    hp = models.IntegerField(null=False)
+    attack = models.IntegerField(null=False)
+    defence = models.IntegerField(null=False)
+    speed = models.IntegerField(null=False)
+
+    def __init__(self, name, kind, owner):
+        super().__init__()
+        self.name = name
+        self.kind = kind
+        self.owner = owner
+        self.hp = ATTRIBUTE[kind]['hp']
+        self.attack = ATTRIBUTE[kind]['attack']
+        self.defence = ATTRIBUTE[kind]['defence']
+        self.speed = ATTRIBUTE[kind]['speed']
